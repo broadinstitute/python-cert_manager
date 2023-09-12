c2ea997e33d8ecc45e9803bd609c337345660ac3
Joachim Burket <joachim.burket@hopitalvs.ch>
2023-09-12 17:16:39 +0200

    first implementation of the person API

--- tests/test_person.py
+++ tests/test_person.py
@@ -3,6 +3,7 @@
 # Don't warn about things that happen as that is part of unit testing
 # pylint: disable=no-member
 
+import json
 from urllib.parse import quote, unquote
 
 from testtools import TestCase
@@ -41,6 +42,32 @@ class TestInit(TestPerson):
         self.assertEqual(person._api_url, self.api_url)  # pylint: disable=protected-access
 
 
+class TestList(TestPerson):
+    """Test the list method"""
+
+    @responses.activate
+    def test_list(self):
+        """The function should return all persons"""
+        # Setup the mocked response
+        test_url = f"{self.api_url}?size=200&position=0"
+        test_result = [
+            {"email": "fry@example.org"}, {"email": "leila@example.org"},
+            {"email": "farnsworth@example.org"}, {"email": "bender@example.org"}
+        ]
+        responses.add(responses.GET, test_url, json=test_result, status=200)
+
+        person = Person(client=self.client)
+        result = person.list()
+
+        # Verify all the query information
+        data = []
+        for res in result:
+            data.append(res)
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, test_url)
+        self.assertEqual(data, test_result)
+
+
 class TestFind(TestPerson):
     """Test the find method."""
 
@@ -49,7 +76,7 @@ class TestFind(TestPerson):
         """The function should return the Person ID corresponding to the email passed."""
         # Setup the mocked response
         test_email = "test@example.com"
-        quoted_email = quote(test_email.replace(".", "%2E"))
+        quoted_email = quote(test_email)
         test_url = f"{self.api_url}/id/byEmail/{quoted_email}"
         test_result = {"personId": 51}
 
@@ -68,7 +95,7 @@ class TestFind(TestPerson):
         """The function should raise an HTTPError exception if a person by that email is not found."""
         # Setup the mocked response
         test_email = "test@example.com"
-        quoted_email = quote(test_email.replace(".", "%2E"))
+        quoted_email = quote(test_email)
         test_url = f"{self.api_url}/id/byEmail/{quoted_email}"
         test_result = {"code": -105, "description": f"Person with e-mail: {unquote(quoted_email)} was not found"}
 
@@ -80,3 +107,123 @@ class TestFind(TestPerson):
         # Verify all the query information
         self.assertEqual(len(responses.calls), 1)
         self.assertEqual(responses.calls[0].request.url, test_url)
+
+
+class TestGet(TestPerson):
+    """Test the get details method"""
+
+    @responses.activate
+    def test_id(self):
+        """The function should return the details of the Person"""
+        # Setup the mocked response
+        test_id = 1234
+        test_url = f"{self.api_url}/{test_id}"
+        test_result = {"firstName": "Fry", "email": "fry@example.org"}
+        responses.add(responses.GET, test_url, json=test_result, status=200)
+
+        person = Person(client=self.client)
+        data = person.get(test_id)
+
+        # Verify all the query information
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, test_url)
+        self.assertEqual(data, test_result)
+
+    @responses.activate
+    def test_not_found(self):
+        """The function should raise an HTTPError exception if a person with an ID is not found."""
+        # Setup the mocked response
+        test_id = 1234
+        test_url = f"{self.api_url}/{test_id}"
+        test_result = {"code": -105, "description": f"Person with ID: {test_id} was not found"}
+        responses.add(responses.GET, test_url, json=test_result, status=404)
+
+        person = Person(client=self.client)
+        self.assertRaises(HTTPError, person.get, id=test_id)
+
+        # Verify all the query information
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, test_url)
+
+
+class TestCreate(TestPerson):
+    """Test the create method"""
+
+    def setUp(self):
+        """Initialize the class."""
+        super().setUp()
+
+        self.test_email = "zoidberg.example.org"
+        self.test_first_name = "Dr."
+        self.test_last_name = "Zoidberg"
+        self.test_org = 1234
+        self.test_url = f"{self.api_url}"
+        self.test_created_id = 12345
+        self.test_result = json.dumps({"personId": str(self.test_created_id)})
+
+    @responses.activate
+    def test_create_success(self):
+        """The function should return a JSON with the `personID` of the person created if a 201 response is given"""
+        responses.add(
+            responses.POST,
+            self.test_url,
+            body=self.test_result,
+            status=201,
+            headers={"Location": f"https://cert-manager.com/api/person/v1/{self.test_created_id}"}
+        )
+
+        person = Person(client=self.client)
+        data = person.create(
+            first_name=self.test_first_name,
+            last_name=self.test_last_name,
+            email=self.test_email,
+            org_id=self.test_org,
+        )
+
+        # Verify all the query information
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, self.test_url)
+        self.assertEqual(json.dumps(data), self.test_result)
+
+
+class TestUpdate(TestPerson):
+    """Test the update method"""
+
+    @responses.activate
+    def test_update_success(self):
+        """The function should return nothing when updating a person"""
+        test_id = 1234
+        test_url = f"{self.api_url}/{test_id}"
+        responses.add(responses.PUT, test_url, status=200)
+
+        person = Person(client=self.client)
+        data = person.update(
+            id=test_id,
+            validation_type="HIGH",
+        )
+
+        # Verify all the query information
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, test_url)
+        self.assertEqual(data, None)
+
+
+class TestDelete(TestPerson):
+    """Test the delete method"""
+
+    @responses.activate
+    def test_delete_success(self):
+        """The function should return nothing when deleting a person"""
+        test_id = 1234
+        test_url = f"{self.api_url}/{test_id}"
+        responses.add(responses.DELETE, test_url, status=200)
+
+        person = Person(client=self.client)
+        data = person.delete(
+            id=test_id,
+        )
+
+        # Verify all the query information
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url, test_url)
+        self.assertEqual(data, None)
