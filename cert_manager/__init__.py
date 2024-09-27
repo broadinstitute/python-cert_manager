fa4f36cd0487f9260135e14e42ab938ce9f5491e
Hirvonen Mikko Vasili <mikko.v.hirvonen@helsinki.fi>
2024-09-27 18:49:15 +0300

    Try to pass automatic tests

--- cert_manager/__init__.py
+++ cert_manager/__init__.py
@@ -5,12 +5,12 @@ from .acme import ACMEAccount
 from .admin import Admin
 from .client import Client
 from .domain import Domain
+from .dcv import DomainControlValidation
 from .organization import Organization
 from .person import Person
 from .report import Report
 from .smime import SMIME
 from .ssl import SSL
-from .dcv import DomainControlValidation
 
 __all__ = [
     "ACMEAccount", "Admin", "Client", "Domain", "DomainControlValidation", 
